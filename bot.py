855ddc69eecae2361a24996a259a53632fa5d54f
valentinnsh <valentinnsh@gmail.com>
2019-04-18 00:45:36 +0300

    Let me introduse Galey

---
+++ bot.py
@@ -0,0 +1,23 @@
+#
+# Copyright (c)
+# 2018 V.Shishkin
+#
+
+# -*- coding: utf-8 -*-
+
+import config
+import telebot
+import sys
+import subprocess
+import time
+
+bot = telebot.TeleBot(config.token)
+
+helping_msg = "Добрый невечер. Я Галея, управляющий, бармен и вообще душа этого места."
+#------Command list --------
+@bot.message_handler(commands=['help'])
+def send_help_msg(message):
+    bot.send_message(message.chat.id, helping_msg)
+
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
