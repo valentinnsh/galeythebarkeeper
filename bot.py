e0a219654e2b27f30c9525ddefba5a3143a91876
valentinnsh <valentinnsh@gmail.com>
2019-04-18 15:52:44 +0300

    fix command help

--- bot.py
+++ bot.py
@@ -17,7 +17,7 @@ bot = telebot.TeleBot(config.token)
 
 helping_msg = "Добрый невечер. Я Галея, управляющий, бармен и вообще душа этого места."
 #------Command list --------
-@bot.message_handler(content_types=['help'])
+@bot.message_handler(commands=['help'])
 def send_help_msg(message):
     bot.send_message(message.chat.id, helping_msg)
 
