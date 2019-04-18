569406e7d99f0be80fcf04258033041d8b206ece
valentinnsh <valentinnsh@gmail.com>
2019-04-18 16:15:21 +0300

    try to fix scilence

--- bot.py
+++ bot.py
@@ -23,5 +23,4 @@ def send_help_msg(message):
 def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
     bot.send_message(message.chat.id, message.text)
 
-if __name__ == '__main__':
-    bot.polling(none_stop=True)
+bot.polling(none_stop=True)
